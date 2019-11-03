d5f1bd2e9ae0fb1564b96160f460e4a294ca7499
sandro <sandrokakashvili@gmail.com>
2019-11-03 18:15:17 +0400

    feat: register

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,59 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom'
 
-const Login = () => (
-  <div>Login</div>
-)
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const { email, password } = formData
+
+  const onFormSubmit = e => {
+    e.preventDefault()
+
+    console.log(formData)
+  }
+
+  const handleFieldChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
+
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user" />
+        Sign Into Your Account
+      </p>
+      <form className="form" onSubmit={e => onFormSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={e => handleFieldChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={e => handleFieldChange(e)}
+            required
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Sign In" />
+      </form>
+      <p className="my-1">
+        Don't have an account ?
+        <Link to="/register"> Sign Up</Link>
+      </p>
+    </Fragment>
+  )
+}
 
 export default Login
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,98 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { setAlert } from '../../actions/alert'
+import { register } from '../../actions/auth'
+import PropTypes from 'prop-types'
 
-const Register = () => (
-  <div>Register</div>
-)
+const Register = ({ setAlert, register }) => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  })
 
-export default Register
+  const { name, email, password, confirmPassword } = formData
+
+  const onFormSubmit = e => {
+    e.preventDefault()
+
+    if (password !== confirmPassword) {
+      setAlert('Passwords do not match', 'danger')
+    } else {
+      register({ name, email, password })
+    }
+  }
+
+  const handleFieldChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
+
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user" />
+        Create Your Account
+      </p>
+      <form className="form" onSubmit={e => onFormSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={e => handleFieldChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={e => handleFieldChange(e)}
+            required
+          />
+          <small className="form-text">
+            This website uses Gravatar so if you want a profile image, use a Gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={e => handleFieldChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            minLength="6"
+            value={confirmPassword}
+            onChange={e => handleFieldChange(e)}
+            required
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account ?
+        <Link to="/login"> Sign In</Link>
+      </p>
+    </Fragment>
+  )
+}
+
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired
+}
+
+export default connect(null, { setAlert, register })(Register)
