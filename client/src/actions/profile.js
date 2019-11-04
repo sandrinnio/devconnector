8f2985ed6460265d856a7c3d3a0cb734db2f28f3
sandro <sandrokakashvili@gmail.com>
2019-11-04 15:59:35 +0400

    feat: edit profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -18,3 +18,36 @@ export const getCurrentProfile = () => async dispatch => {
     })
   }
 }
+
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    const res = await axios.post(`${apiUrl}/api/profile`, formData, config)
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    })
+
+    dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'))
+
+    if (!edit) {
+      history.push('/dashboard')
+    }
+  } catch (error) {
+    const errors = error.response.data.errors
+
+    if (errors) {
+      errors.map(error => dispatch(setAlert(error.msg, 'danger')))
+    }
+
+    dispatch({
+      type: PROFILE_FAILURE,
+      payload: { msg: error.response.data.msg, status: error.response.status }
+    })
+  }
+}
