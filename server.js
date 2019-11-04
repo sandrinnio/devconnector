f071611b33f927f89e1c090b8f6c42acdc2d45fa
sandro <sandrokakashvili@gmail.com>
2019-11-05 00:11:39 +0400

    chore: deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express()
 
@@ -10,12 +11,18 @@ app.use(cors())
 
 app.use(express.json({ extended: false }))
 
-app.get('/', (req, res) => res.send('API Running'))
-
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5000
 
